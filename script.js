8588cec8e49061c8c33bcf3a9c6121ed076fc9f6
sanjaysushilkumarbenjamin-pixel <sanjaysushilkumarbenjamin@gmail.com>
2026-05-05 16:17:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("bg");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for (let i = 0; i < 100; i++) {
+  particles.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    size: Math.random() * 2,
+    speedX: Math.random() - 0.5,
+    speedY: Math.random() - 0.5
+  });
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  particles.forEach(p => {
+    p.x += p.speedX;
+    p.y += p.speedY;
+
+    ctx.fillStyle = "cyan";
+    ctx.fillRect(p.x, p.y, p.size, p.size);
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
