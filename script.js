19652716e54a7a3f432cec073f9533029f65c3db
sanjaysushilkumarbenjamin-pixel <sanjaysushilkumarbenjamin@gmail.com>
2026-05-06 17:35:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -282,3 +282,12 @@ gsap.from(".bio-content", {
   x: 100,
   duration: 1.2
 });
+
+const glow = document.querySelector(".cursor-glow");
+
+window.addEventListener("mousemove", (e) => {
+
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+
+});
