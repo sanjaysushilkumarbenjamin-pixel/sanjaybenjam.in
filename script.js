9d8a7806a2914b917b12048c570d7caf0eb943ef
sanjaysushilkumarbenjamin-pixel <sanjaysushilkumarbenjamin@gmail.com>
2026-05-06 18:19:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -377,3 +377,47 @@ buttons.forEach(btn => {
   });
 
 });
+
+/* ===================================
+COUNTERS
+=================================== */
+
+const counters =
+  document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+  const updateCounter = () => {
+
+    const target =
+      +counter.getAttribute("data-target");
+
+    const current =
+      +counter.innerText;
+
+    const increment =
+      target / 100;
+
+    if (current < target) {
+
+      counter.innerText =
+        Math.ceil(current + increment);
+
+      setTimeout(updateCounter, 20);
+
+    } else {
+
+      counter.innerText = target;
+    }
+  };
+
+  ScrollTrigger.create({
+
+    trigger: counter,
+
+    start: "top 85%",
+
+    onEnter: updateCounter
+  });
+
+});
